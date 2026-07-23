1f5d71b58e57e202497d0b70fb7fc6f4df68e777
Parag jain <iesparagjain@gmail.com>
2026-07-23 19:05:49 +0530

    backend url's

---
+++ customerfacing/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://market-place-production-991b.up.railway.app/api/v1',
+  socketUrl: 'https://market-place-production-991b.up.railway.app',
+};
